a086e525b3a3f3ccdf640e574de7a21a6fe115f9
commit111 <llee008844@gmail.com>
2025-04-02 17:00:12 -0700

    remove challenge command

--- utils.js
+++ utils.js
@@ -41,7 +41,3 @@ export function getRandomEmoji() {
   const emojiList = ['😄','😌','🤓','😎','🤖','🌏','👋','🌊','✨'];
   return emojiList[Math.floor(Math.random() * emojiList.length)];
 }
-
-export function capitalize(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
